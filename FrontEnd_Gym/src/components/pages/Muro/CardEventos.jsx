36fa56c5a51e4a776c00d0ed82cfc5c1277afd51
Elvis Alas <110005376+ElvisAlas@users.noreply.github.com>
2024-05-28 23:08:35 -0600

    modificando los card del muro

--- FrontEnd_Gym/src/components/pages/Muro/CardEventos.jsx
+++ FrontEnd_Gym/src/components/pages/Muro/CardEventos.jsx
@@ -5,6 +5,7 @@ export const CardEventos = () => {
   const [dataMuro, setDataMuro] = useState([]);
   const [contador, setContador] = useState(0);
   const [isloading, setIsLoading] = useState(true);
+  const [likedEvents, setLikedEvents] = useState([]);
 
   const getDatos = async () => {
     try {
@@ -22,22 +23,76 @@ export const CardEventos = () => {
     getDatos();
   }, [contador]);
 
+  const handleLike = (id) => {
+    setLikedEvents((prevLikedEvents) => {
+      if (prevLikedEvents.includes(id)) {
+        console.log(likedEvents)
+        return prevLikedEvents.filter((eventId) => eventId !== id);
+      } else {
+        console.log(likedEvents)
+        return [...prevLikedEvents, id];
+      }
+      
+    });
+  };
+
   return (
     <>
-      <div className="col-12 col-sm-12 col-md-12 col-lg-12 px-0">
+      <div className="col-12 px-0">
         {isloading ? (
-          <div className="d-flex justify-content-center mt-5 mb-5">
+          <div className="d-flex justify-content-center ">
             <div className="spinner-border text-primary" role="status">
               <span className="visually-hidden"></span>
             </div>
           </div>
         ) : (
           dataMuro.map((evento) => (
-            <div
-              key={evento.id}
-              className="col-lg-12 col-md-12 col-sm-12 col-xs-12"
-            >
-              <div className="card bg-eventos mt-3">
+            <div className="upcoming-events" key={evento.id}>
+              <div className="event-container">
+                <div className="card event-card">
+                  <div className="event-header">
+                    <img
+                      src={`data:${evento.mime_type};base64,${evento.foto}`}
+                      alt=""
+                    />
+                    <p>
+                      {evento.fecha
+                        ? evento.fecha.slice(0, 10)
+                        : "Fecha no disponible"}
+                    </p>
+                    <i
+                      className={
+                        likedEvents.includes(evento.id)
+                          ? "bx bx-heart like-btn bounce-in fa-solid fa-heart" 
+                          : "bx bxs-heart like-btn fa-solid fa-heart"
+                      }
+                      onClick={() => handleLike(evento.id)}
+                    ></i>
+                  </div>
+                  <div className="event-content">
+                    <h2>{evento.nombre}</h2>
+                    <p
+                      style={{
+                        whiteSpace: "normal",
+                        wordBreak: "break-word",
+                      }}
+                    >
+                      {evento.descripcion}
+                    </p>
+                  </div>
+                  <div className="event-footer">
+                    <p>
+                      <i className="fa-solid fa-comment"></i>{" "}
+                    </p>
+                    <p>
+                      <i className="fa-solid fa-bookmark"></i>{" "}
+                    </p>
+                    <p>
+                      <i className="fa-solid fa-heart"></i>
+                    </p>
+                  </div>
+
+                  {/* <div className="card bg-eventos mt-3">
                 <img
                   src={`data:${evento.mime_type};base64,${evento.foto}`}
                   className="muro-evento"
@@ -56,7 +111,9 @@ export const CardEventos = () => {
                     {evento.descripcion}
                   </p>
                   <div className="gap-2 d-flex justify-content-between border-top pt-2">
-                    <span className="text-white"><i class="fa-solid fa-heart"></i> 1</span>
+                    <span className="text-white">
+                      <i class="fa-solid fa-heart"></i> 1
+                    </span>
                     <span className="text-white">2 comentarios</span>
                   </div>
                   <div className="d-grid gap-2 d-flex">
@@ -81,6 +138,8 @@ export const CardEventos = () => {
                       Guardar <i class="fa-solid fa-bookmark"></i>
                     </button>
                   </div>
+                </div>
+              </div> */}
                 </div>
               </div>
             </div>
